c27733d1b19a5206eefdf040fc09db012660e48f
ShenBen <shenben19970221>
2021-09-05 21:07:24 +0800

    拆分推流demo

---
+++ push/src/main/java/com/shencoder/webrtc_srs/push/http/bean/SrsRequestBean.kt
@@ -0,0 +1,11 @@
+package com.shencoder.webrtc_srs.push.http.bean
+
+
+import com.squareup.moshi.Json
+
+data class SrsRequestBean(
+    @Json(name = "sdp")
+    val sdp: String?,
+    @Json(name = "streamurl")
+    val streamUrl: String?
+)
---
+++ push/src/main/java/com/shencoder/webrtc_srs/push/http/bean/SrsResponseBean.kt
@@ -0,0 +1,17 @@
+package com.shencoder.webrtc_srs.push.http.bean
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SrsResponseBean(
+    @Json(name = "code")
+    val code: Int,
+    @Json(name = "sdp")
+    val sdp: String?,
+    @Json(name = "server")
+    val server: String?,
+    @Json(name = "sessionid")
+    val sessionId: String?
+)
